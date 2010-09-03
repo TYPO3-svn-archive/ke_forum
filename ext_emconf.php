43818ea43deb2fce5b182b8ad5f6bc5ba49f4a3b
kiefer <kiefer@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-03 10:55:39 +0000

    - ext info updated for TER release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ke_forum/trunk@37642 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Forum',
-	'description' => '',
+	'description' => 'Forum software with basic functions',
 	'category' => 'plugin',
 	'author' => 'Andreas Kiefer (kennziffer.com)',
 	'author_email' => 'kiefer@kennziffer.com',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'www.kennziffer.com GmbH',
-	'version' => '0.0.0',
+	'version' => '0.0.1',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
